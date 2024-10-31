05b3eaa23127c9f7fc212379eed69de15a8e2dac
S-JaeWon <ssm5170@gmail.com>
2024-10-31 20:00:13 +0900

    first commit

---
+++ src/test/java/com/team4/learning_partners/LearningPartnersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4.learning_partners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningPartnersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
